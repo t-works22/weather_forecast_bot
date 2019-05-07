cff88cd40734aad3fe9e149c9e3158c7f5be9f09
M-T <45688679+M-T-work@users.noreply.github.com>
2019-05-07 13:05:30 +0900

    Change all codes to exemples

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -1,37 +1,38 @@
-class LinebotsController < ApplicationController
+class LinebotController < ApplicationController
   require 'line/bot'  # gem 'line-bot-api'
   require 'open-uri'
   require 'kconv'
   require 'rexml/document'
 
+  # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
 
   def callback
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
-
     unless client.validate_signature(body, signature)
       error 400 do 'Bad Request' end
     end
-
     events = client.parse_events_from(body)
-
     events.each { |event|
       case event
+        # メッセージが送信された場合の対応（機能①）
       when Line::Bot::Event::Message
         case event.type
+          # ユーザーからテキスト形式のメッセージが送られて来た場合
         when Line::Bot::Event::MessageType::Text
-          # event.message['text']: ユーザーから送られてきたメッセージ
+          # event.message['text']：ユーザーから送られたメッセージ
           input = event.message['text']
           url  = "https://www.drk7.jp/weather/xml/13.xml"
-          xml = open(url)
+          xml  = open( url ).read.toutf8
           doc = REXML::Document.new(xml)
           xpath = 'weatherforecast/pref/area[4]/'
-
+          # 当日朝のメッセージの送信の下限値は20％としているが、明日・明後日雨が降るかどうかの下限値は30％としている
           min_per = 30
-
           case input
-          when /.*(明日||あした).*/
+            # 「明日」or「あした」というワードが含まれる場合
+          when /.*(明日|あした).*/
+            # info[2]：明日の天気
             per06to12 = doc.elements[xpath + 'info[2]/rainfallchance/period[2]'].text
             per12to18 = doc.elements[xpath + 'info[2]/rainfallchance/period[3]'].text
             per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text
@@ -42,7 +43,7 @@ class LinebotsController < ApplicationController
               push =
                   "明日の天気？\n明日は雨が降らない予定だよ(^^)\nまた明日の朝の最新の天気予報で雨が降りそうだったら教えるね！"
             end
-          when /.*(明後日||あさって).*/
+          when /.*(明後日|あさって).*/
             per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]l'].text
             per12to18 = doc.elements[xpath + 'info[3]/rainfallchance/period[3]l'].text
             per18to24 = doc.elements[xpath + 'info[3]/rainfallchance/period[4]l'].text
@@ -80,6 +81,7 @@ class LinebotsController < ApplicationController
                   "今日の天気？\n今日は雨は降らなさそうだよ。\n#{word}"
             end
           end
+          # テキスト以外（画像等）のメッセージが送られた場合
         else
           push = "テキスト以外はわからないよ〜(；；)"
         end
@@ -88,11 +90,15 @@ class LinebotsController < ApplicationController
             text: push
         }
         client.reply_message(event['replyToken'], message)
+        # LINEお友達追された場合（機能②）
       when Line::Bot::Event::Follow
+        # 登録したユーザーのidをユーザーテーブルに格納
         line_id = event['source']['userId']
         User.create(line_id: line_id)
+        # LINEお友達解除された場合（機能③）
       when Line::Bot::Event::Unfollow
-        line_id = event['source']['UserId']
+        # お友達解除したユーザーのデータをユーザーテーブルから削除
+        line_id = event['source']['userId']
         User.find_by(line_id: line_id).destroy
       end
     }
